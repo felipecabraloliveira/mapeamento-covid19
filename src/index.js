9d54cdfce0d447c496b35b23cf7be0563055687f
Felipe Cabral <felipecabral33@gmail.com>
2022-05-26 22:14:37 -0300

    Desafio Mapeamento Covid 19 - Ajuste PWA

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+/*
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
