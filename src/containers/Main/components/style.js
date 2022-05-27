382f5c783b393218b79ba28adb48d1698be0be21
Felipe Cabral <felipecabral33@gmail.com>
2022-05-26 22:26:10 -0300

    Desafio Mapeamento Covid 19 - Ajuste PWA

--- src/containers/Main/components/style.js
+++ src/containers/Main/components/style.js
@@ -1,38 +1,33 @@
-import styled from "styled-components";
-import { CardContent, Typography } from "../../../components";
+import styled from 'styled-components'
+import { CardContent, Typography } from '../../../components'
 
 export const LabelStyled = styled(Typography)`
   font-weight: 500;
   font-size: 1.5rem;
-`;
+`
 
 export const ValueStyled = styled(Typography)`
-  font-weight: 400;
-  font-size: 2.5rem;
-`;
+  font-weight: 500;
+  font-size: 2.2rem;
+`
 
 export const CardContentStyled = styled(CardContent)`
-  border-left: 8px solid ${({ color }) => color || "#5d78ff"};
-`;
+  border-left: 8px solid ${({ color }) => color || '#5d78ff'};
+`
 
 export const CardPanelContentStyled = styled(CardContent)`
   display: flex;
   justify-content: space-between;
   padding: 25px;
-  @media (max-width: 480px) {
-    flex-direction: column;
+  @media (max-width: 480px){	  
+	  flex-direction: column-reverse;   
   }
-`;
+`
+
 
 export const ItemStyled = styled.div`
   display: flex;
   justify-content: space-between;
-  min-width: 200px;
-`;
+  min-width: 150px;
+`
 
-export const DivStyled = styled.div`
-  @media (max-width: 480px) {
-    margin: auto;
-    padding-top: 15px;
-  }
-`;
